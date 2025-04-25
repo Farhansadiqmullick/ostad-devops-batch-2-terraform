885f801001e814526e0f60bc201bccc392d0ce79
Farhan Mullick <38060872+Farhansadiqmullick@users.noreply.github.com>
2025-04-26 00:29:26 +0600

    terraform init

--- main.tf
+++ main.tf
@@ -1,15 +1,24 @@
+variable "access_key" {
+  type = string
+}
+
+variable "secret_key" {
+  type = string
+}
+
+
+
 terraform {
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "5.96.0"
+    alicloud = {
+      source  = "aliyun/alicloud"
+      version = "~> 1.212.0"
     }
   }
 }
 
-# Configure the AWS Provider
-provider "aws" {
-  region = "ap-southeast-1"
-
+provider "alicloud" {
+  region     = "ap-southeast-1a" 
+  access_key = var.access_key
+  secret_key = var.secret_key
 }
-
